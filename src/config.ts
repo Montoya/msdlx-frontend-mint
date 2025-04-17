954d4432aab213cfcddfba75450a1cea3a3350c0
Christian Montoya <christian.montoya@consensys.net>
2025-04-16 22:25:19 -0400

    init

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+const config = {
+  chain: {
+    base: {
+      provider: 'https://base-mainnet.g.alchemy.com/v2/9_NGmHg4QVRvYJtREjfpehF3FYQY0vvp',
+      contract: '0x03C6Da1c0F5E4219852a000F32520cde3B1AbcF6' 
+    }
+  },
+  gelato: 'https://api.gelato.digital'
+};
+
+export default config;
